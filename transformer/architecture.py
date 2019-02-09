20b2dcba9d6a85eff3baae570706a4385cdeff5e
“rauf-kurbanov” <kurbanov.re@gmail.com>
2019-02-10 00:29:36 +0300

    top level architecture

---
+++ transformer/architecture.py
@@ -0,0 +1,33 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class EncoderDecoder(nn.Module):
+
+    def __init__(self, encoder, decoder, src_embed, tgt_embed, generator):
+        super(EncoderDecoder, self).__init__()
+
+        self.encoder = encoder
+        self.decoder = decoder
+        self.src_embed = src_embed
+        self.tgt_embed = tgt_embed
+        self.generator = generator
+
+    def forward(self, src, tgt, src_mask, tgt_mask):
+        self.decode(self.encode(src, src_mask), src_mask, tgt, tgt_mask)
+
+    def encode(self, src, src_mask):
+        pass
+
+    def decode(self, memory, src_mask, tgt, tgt_mask):
+        pass
+
+
+class Generator(nn.Module):
+
+    def __init__(self, d_model, vocab):
+        super(Generator, self).__init__()
+        self.proj = nn.Linear(d_model, vocab)
+
+    def forward(self, x):
+        return F.log_softmax(self.proj(x), dim=-1)
