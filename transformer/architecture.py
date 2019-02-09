1b127681dcedb365f125c24bf58b90b0c6bb11fd
“rauf-kurbanov” <kurbanov.re@gmail.com>
2019-02-10 00:51:59 +0300

    encoder

--- transformer/architecture.py
+++ transformer/architecture.py
@@ -1,5 +1,5 @@
-import torch.nn as nn
 import torch.nn.functional as F
+from torch import nn
 
 
 class EncoderDecoder(nn.Module):
---
+++ transformer/encoder.py
@@ -0,0 +1,45 @@
+import copy
+
+from torch import nn
+from torch.nn import LayerNorm
+
+
+def clones(module, N):
+    return nn.ModuleList([copy.deepcopy(module) for _ in range(N)])
+
+
+class Encoder(nn.Module):
+
+    def __init__(self, layer, N):
+        super(Encoder, self).__init__()
+        self.layers = clones(layer, N)
+        self.norm = LayerNorm(layer.size)
+
+    def forward(self, x, mask):
+        for layer in self.layers:
+            x = layer(x, mask)
+        return self.norm(x)
+
+
+class SublayerConnection(nn.Module):
+
+    def __init__(self, size, dropout):
+        super(SublayerConnection, self).__init__()
+        self.norm = LayerNorm(size)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, sublayer):
+        return x + self.dropout(sublayer(self.norm(x)))
+
+
+class EncoderLayer(nn.Module):
+    def __init__(self, size, self_attn, feed_forward, dropout):
+        super(EncoderLayer, self).__init__()
+        self.self_attn = self_attn
+        self.feed_forward = feed_forward
+        self.sublayer = clones(SublayerConnection(size, dropout), 2)
+        self.size = size
+
+    def forward(self, x, mask):
+        x = self.sublayer[0](x, lambda t: self.self_attn(t, t, t, mask))
+        return self.sublayer[1](x, self.feed_forward)
