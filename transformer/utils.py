5cb61355a8c1e8932ef1bc04d727a7c0f6401413
“rauf-kurbanov” <kurbanov.re@gmail.com>
2019-02-22 04:05:32 +0300

    multihead attention

--- transformer/utils.py
+++ transformer/utils.py
@@ -1,11 +1,20 @@
 import copy
 
+import numpy as np
+import torch
 from torch import nn
 from torch.nn import LayerNorm
 
 
 def clones(module, N):
     return nn.ModuleList([copy.deepcopy(module) for _ in range(N)])
+
+
+def subsequent_mask(size):
+    """Mask out subsequent positions."""
+    attn_shape = (1, size, size)
+    subsequent_mask = np.triu(np.ones(attn_shape), k=1).astype('uint8')
+    return torch.from_numpy(subsequent_mask) == 0
 
 
 class SublayerConnection(nn.Module):
