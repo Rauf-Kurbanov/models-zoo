b63afc6dfd16ef4d46d7cc88b781516eaee42633
“rauf-kurbanov” <kurbanov.re@gmail.com>
2019-02-22 20:48:42 +0300

    per-word feedforward

--- transformer/utils.py
+++ transformer/utils.py
@@ -26,3 +26,15 @@ class SublayerConnection(nn.Module):
 
     def forward(self, x, sublayer):
         return x + self.dropout(sublayer(self.norm(x)))
+
+
+class PositionwiseFeedForward(nn.Module):
+
+    def __init__(self, d_model, d_ff, dropout=0.1):
+        super(PositionwiseFeedForward, self).__init__()
+        self.w_1 = nn.Linear(d_model, d_ff)
+        self.w_2 = nn.Linear(d_ff, d_model)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        return self.w_2(self.dropout(F.relu(self.w_1(x))))
